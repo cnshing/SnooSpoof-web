4a7d7c30a7dbc6bec3caac061ef9c206ae5872d2
Shing Chan <chan.shing@protonmail.com>
2022-11-14 13:13:22 +0000

    Scale larger font size of title to better match initial design and fix spacing on the text

--- src/app/title.tsx
+++ src/app/title.tsx
@@ -4,7 +4,7 @@ export default function Title() {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Generate reddit posts of any user through
-        <em className={styles.emphasize_blue}>AI</em>
+        <em className={styles.emphasize_blue}> AI </em>
       </h1>
     </div>
   )
