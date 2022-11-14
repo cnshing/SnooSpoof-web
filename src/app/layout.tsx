14a0c4dcd8388789be5d353ed0e26109c5bf7401
Shing Chan <chan.shing@protonmail.com>
2022-11-14 13:16:36 +0000

    Implemet top right corner icon positioning

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css'
+import Icons from "@/app/tabs"
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      {/*
+        <head /> will contain the components returned by the nearest parent
+        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+      <head />
+      <body>
+        <Icons />
+        <div>{children}</div>
+      </body>
+    </html>
+  )
+}
