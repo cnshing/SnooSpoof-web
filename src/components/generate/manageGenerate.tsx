2e8a83bc782bc7df9512c98f16fcf5dc606a2cd9
Shing Chan <chan.shing@protonmail.com>
2023-03-10 11:49:09 +0000

    Implement general purpose Input fields, populate sidebar with settings that saves itself on close

---
+++ src/components/generate/manageGenerate.tsx
@@ -0,0 +1,81 @@
+'use client';
+/* State managment for generating text */
+
+export type GenerationConfig = {
+    username: string
+    subreddit: string | undefined
+    prompt: string | undefined
+    comments_only: boolean
+    is_original_content: boolean
+    over_18: boolean
+  }
+  
+  export type GeneratedText = GenerationConfig & { response: string | undefined }
+  
+  export const defaultConfig: GenerationConfig = {
+    username: "",
+    subreddit: undefined,
+    prompt: undefined,
+    comments_only: false,
+    is_original_content: false,
+    over_18: false
+  }
+  
+  export const defaultText: GeneratedText = {
+    username: "Snoobot",
+    subreddit: "SnooSpoof",
+    prompt: "Sorry, but we are unable to retrieve the generated text.",
+    response: "¯\_(ツ)_/¯",
+    comments_only: false,
+    is_original_content: true,
+    over_18: false
+  }
+  
+  /*
+  From
+  https://stackoverflow.com/questions/69464179/how-to-extract-keys-of-certain-type-from-object
+  */
+  type ObtainKeys<Obj, Type> = {
+    [Prop in keyof Obj]: Obj[Prop] extends Type ? Prop : never
+  }[keyof Obj]
+  
+  /**
+   * The keys of GenerationConfig, filterable by type T.
+   * 
+   * @template T If T is specificed, return only the keys of that type in GenerationConfig
+   */
+  export type GenerationKeys<T = any> = ObtainKeys<GenerationConfig, T>
+  export type GenerationValues = GenerationConfig[keyof GenerationConfig]
+  
+  /**
+   * Retrieves the configuration for generating text from local storage. Follows the API types GenerationConfig.
+   * @param setting Any setting in GenerationConfig
+   * @returns A string representing the requested setting. If no such setting is defined, return an empty string.
+   * If multiple settings are requested, return a key-value object of the same expected returns above.
+   */
+  export function getSetting(setting: GenerationKeys | string[]) {
+    function getSingleSetting(setting: GenerationKeys) {
+      const value = window.localStorage.getItem(setting)
+      return value !== null ? JSON.parse(value) : value;
+    }
+    if (Array.isArray(setting)) {
+      return setting.reduce((settings, eachSetting) => ({ ...settings, [eachSetting]: getSingleSetting(eachSetting) }), {})
+    }
+    return getSingleSetting(setting);
+  }
+  
+  /**
+   * Updates the configuration for generating text to local storage. Follows the API types GenerationConfig.
+   * @param setting Any setting in GenerationConfig
+   * @param value Any allowed values of the selected setting in GenerationConfig
+   */
+  export function updateSetting(setting: GenerationKeys, value: GenerationValues) {
+    window.localStorage.setItem(setting, JSON.stringify(value));
+  }
+  
+  /**
+   * Retrieves the total configuration for generating text from local storage.
+   */
+  export function getGenerationConfig() {
+    return getSetting(Object.keys(defaultConfig))
+  }
--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,11 +1,15 @@
 'use client';
 import styles from "@/components/sidebar/sidebar.module.scss"
-import { useRef, useCallback, useEffect } from "react";
+import { useRef, useCallback, useEffect, useMemo } from "react";
 import { manageSidebar } from "@/components/sidebar/manageSidebar"
+import { updateSetting, getSetting, GenerationKeys, GenerationValues, GenerationConfig, defaultConfig } from "@/components/generate/manageGenerate"
+import Article from "@/components/post/article"
+import { Input, Textarea, Checkbox } from "@/components/ui/fields"
 
 
 export default function Sidebar({ isVisible, closeSettings }: Partial<manageSidebar>) {
 
+  /* Animations */
   const sidebarRef = useRef<HTMLElement>(null);
   const sidebar: HTMLElement | null = sidebarRef.current;
   const exitSidebar = useCallback(
@@ -37,12 +41,69 @@ export default function Sidebar({ isVisible, closeSettings }: Partial<manageSide
     [isVisible]
   )
 
+
   const animateSidebar: string = isVisible ? styles.openSidebar : styles.closeSidebar;
+
+  /* Look and feel */
+  const articleSize: string = "clamp(150%, 1.4375rem, 1.75rem)"
+  /* At 1920 px width, set to 1.75rem. Linearly transition to 1.1875 rem at 1010px width.*/
+  const checkboxSize: string = "clamp(1.1875rem, 2.374313186813187rem + -0.9890109890109889vw, 1.75rem)"
+  /* Retrieve the settings saved from the previous session*/
+  const settings: Omit<GenerationConfig, 'username'> = useMemo( //useMemo ensures these settings are fetched only once on first load
+    () => {
+      /* 
+      Retrieves the following settings into a JS object:
+      https://stackoverflow.com/questions/1117916/merge-keys-array-and-values-array-into-an-object-in-javascript
+      */
+
+      const { username, ...initialSettings } = defaultConfig;
+
+      return getSetting(Object.keys(initialSettings))
+
+    },
+    []);
+
+  const saveOption = (setting: GenerationKeys) => (value: GenerationValues) => {
+    updateSetting(setting, value);
+    return value;
+  }
+
+  const loadCheckbox = (setting: GenerationKeys<boolean>): boolean => {
+    /* When loading the checkbox from local storage, new users will have never saved the
+    settings to local storage. Thus, getSettings will return "null" as no such data exists. 
+    In that case, simply return the default configuration. This function
+    should not be required if the initial loading of our stored data checks for these null values.
+    */
+    const value: boolean = settings[setting]
+    return value === null ? defaultConfig[setting] : value
+  }
 
   return (
     <aside ref={sidebarRef} className={`${styles.sidebar} ${animateSidebar}`}>
-      <section>Test1</section>
-      <section>Test2</section>
+      <div>
+        <Article headline="Subreddit"
+          size={articleSize}>
+          <p>Lets the text correspond to a given subreddit. A valid subreddit is not required.</p>
+        </Article>
+        {/* FIXME: Ensure type compatibility with general input value function and save function */}
+        <Input initialValue={settings['subreddit']} composeValue={saveOption("subreddit")} placeholder="all" />
+      </div>
+      <div>
+        <Article headline="Autofill"
+          size={articleSize}>
+          <p>Autocompletes the initial prompt. Enable "Comments Only" to reply to your prompt instead.</p>
+        </Article>
+        <Textarea initialValue={settings["prompt"]} composeValue={saveOption("prompt")} style={{ height: "10rem" }} placeholder="My prompt goes here..." />
+      </div>
+      <div>
+        <Article headline="Tags"
+          size={articleSize}>
+          <p>Shift content to be more associated with these tags.</p>
+        </Article>
+        <Checkbox initialChecked={loadCheckbox("comments_only")} composeChecked={saveOption("comments_only")} label="Comments Only" checkboxSize={checkboxSize} />
+        <Checkbox initialChecked={loadCheckbox("is_original_content")} composeChecked={saveOption("is_original_content")} label="Original Content" checkboxSize={checkboxSize} />
+        <Checkbox initialChecked={loadCheckbox("over_18")} composeChecked={saveOption("over_18")} label="NSFW" checkboxSize={checkboxSize} />
+      </div>
     </aside>
   )
 }
