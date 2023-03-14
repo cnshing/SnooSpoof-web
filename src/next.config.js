3a5c070e704455374030f46c798d2d2311a96f4a
Shing Chan <chan.shing@protonmail.com>
2023-03-14 09:43:59 +0000

    Temporarily remove next export until it is supported in NextJS 13

--- src/next.config.js
+++ src/next.config.js
@@ -3,7 +3,10 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-  output: 'export',
+  // NextJS 13 app directory still does not fully support Static Site Generation.
+  // See the roadmap for the current status.
+  // https://beta.nextjs.org/docs/app-directory-roadmap
+  //output: 'export',
   typescript: {
     ignoreBuildErrors: true,
   }
