ee6b8a562870aed17d46005934ca7af09ba42ed4
Shing Chan <chan.shing@protonmail.com>
2023-04-28 05:48:28 +0000

    refactor: Move clear:right as module class and add Snoobott container styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,8 @@ import Icons from "@/app/tabs"
 import Github from "@/app/github"
 import Gear from "@/components/tabs/gear"
 import gear from "@/public/gear.svg"
+import { CSSProperties } from 'react';
+import styles from "@/app/layout.module.css"
 
 const noto_sans = Noto_Sans_Display({
   subsets: ['latin'],
@@ -43,8 +45,7 @@ export default function RootLayout({
           </Icons>
           <Sidebar requires={['isVisible', 'closeSettings']}/>
         </ShareSidebar>
-        {/* Clear right to ensure floated elements stay at the correct position */}
-        <div style={{clear: "right"}}>{children}</div>
+        <div className={`${styles.clearTabs} ${styles.snoobot}`}>{children}</div>
       </body>
     </html>
   )
